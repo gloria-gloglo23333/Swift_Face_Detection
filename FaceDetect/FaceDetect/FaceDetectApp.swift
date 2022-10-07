33ab6745661de4e24fa7c6216bca3eb354e796cd
Gloria Guo <73170220+gloria-gloglo23333@users.noreply.github.com>
2022-10-07 17:22:32 +0100

    Add files via upload

---
+++ FaceDetect/FaceDetect/FaceDetectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceDetectApp.swift
+//  FaceDetect
+//
+//  Created by Gloria Guo on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FaceDetectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
